17580d50e2cdeb8fa6c676f051dfe0e8fc01ada7
Derek Mercedes <mercedes.derek@gmail.com>
2023-11-25 11:33:55 -0700

    random dogs

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,28 @@
 // create your App component here
+import React from "react"
+
+function App(){
+
+    const [dog, setDog] = React.useState("")
+
+    React.useEffect(()=>{
+        fetch("https://dog.ceo/api/breeds/image/random")
+        .then(r=>r.json())
+        .then(data=>{
+            setDog(data.message)
+        })
+    }, [])
+
+    if(dog === ""){
+        return (
+            <p>Loading...</p>
+        )
+    }else{
+        return (
+            <img src={dog} alt="A Random Dog" />
+        )
+        
+    }
+}
+
+export default App
